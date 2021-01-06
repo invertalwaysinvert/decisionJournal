6a48a5abedb05ed24d020e68c6a624342039c91f
Vinayak Kaushik <vinayakkaushik@protonmail.com>
2021-01-06 14:36:45 +0530

    Adds CSS framework

--- journal/src/App.js
+++ journal/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import '../node_modules/milligram/dist/milligram.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<h1>Heading</h1>
+<h2>Heading</h2>
+<h3>Heading</h3>
+<h4>Heading</h4>
+<h5>Heading</h5>
+<h6>Heading</h6>
+
+<a class="button" href="#">Predict</a>
     </div>
   );
 }
