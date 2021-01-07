76499cd36371a2234fc1c2034dd973785735d272
Vinayak Kaushik <vinayakkaushik@protonmail.com>
2021-01-07 12:53:53 +0530

    Adds homepage skeleton

--- journal/src/App.js
+++ journal/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
-import '../node_modules/milligram/dist/milligram.css';
+import "../node_modules/milligram/dist/milligram.css";
+import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
-    <div className="App">
-
-<h1>Heading</h1>
-<h2>Heading</h2>
-<h3>Heading</h3>
-<h4>Heading</h4>
-<h5>Heading</h5>
-<h6>Heading</h6>
-
-<a class="button" href="#">Predict</a>
+    <div className="homepage row">
+      <div className="column column-20">
+        <div className="title">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+      <div className="column">
+        <div className="content">
+          <a href="/asdf" className="button button-black">
+            Get Started
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
